ab4701f8f54c543c552dc07791b43e4c25c3042e
sgpl <sharad45@gmail.com>
2013-12-05 12:37:46 +0300

    added notes

--- chapter3/boolean_golf.rb
+++ chapter3/boolean_golf.rb
@@ -37,3 +37,16 @@ so methods we add here will be available to every variable of any kind.
 =end
 
 # method that returns a boolean is a predicate. 
+
+# :to_b is an instance of the Symbol class. which are preceded with a colon. 
+# metaprogramming: general term for writing programs that create/manipulate other programs. 
+# technically a compiler or interpreter is an ex. of metaprogramming
+# because it allows you to write programs in a high level lang eg: ruby
+# which creates a program in a low language eg: C that gets executed. 
+
+# eg: 
+
+# class String
+# 	alias :make_me_into_an_integer :to_i
+# end
+
