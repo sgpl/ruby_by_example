8ef062fdd8641b4dad33bde6187385b4f3eaa243
sgpl <sharad45@gmail.com>
2014-02-19 01:22:04 +0300

    script to get line numbers from files from the command line

---
+++ chapter4/line_num.rb
@@ -0,0 +1,19 @@
+#!/usr/bin/env ruby 
+
+def get_lines(filename)
+	return File.open(filename, 'r').readlines
+end
+
+def get_format(lines)
+	return "%0#{lines.size.to_s.size}d"
+end
+
+def get_output(lines)
+	format = get_format(lines)
+	output = ''
+	lines.each_with_index do |line,i|
+	output += "#{sprintf(format, i+1)}: #{line}" end
+	return output 
+end
+
+print get_output(get_lines(ARGV[0]))
